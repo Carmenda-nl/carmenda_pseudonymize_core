62eb732d727e7f54984135d70f0303777ec97c96
Django Heimgartner <django.heimgartner@gmail.com>
2025-03-07 14:56:54 +0100

    Set api handler with endpoint

--- api/views.py
+++ api/views.py
@@ -1,14 +1,55 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from .models import DeidentificationJob
+from .serializers import DeidentificationJobSerializer
+from deduce_service.deduce_manager import process_deidentification
+import threading
 
 
-class ApiViewSet(viewsets.ViewSet):
-    @action(methods=['GET'],  detail=False, name='Get Value from input')
-    def get_val_from(self, request):
+class DeidentificationJobViewSet(viewsets.ModelViewSet):
+    queryset = DeidentificationJob.objects.all()
+    serializer_class = DeidentificationJobSerializer
 
-        input = request.GET['input']
+    def create(self, request, *args, **kwargs):
+        """
+        Override create method to accept file and run the job.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        job = serializer.save()
 
-        return Response(
-            status=status.HTTP_200_OK, data=f'{input}, value from Django'
+        # Start process in a seperate thread
+        thread = threading.Thread(
+            target=process_deidentification,
+            args=(job.id,)
         )
+        thread.daemon = True
+        thread.start()
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+    @action(detail=True, methods=['get'])
+    def check_status(self, request, pk=None):
+        """Check the status of the job."""
+        job = self.get_object()
+        serializer = self.get_serializer(job)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'])
+    def download(self, request, pk=None):
+        """Download the results if available."""
+        job = self.get_object()
+
+        if job.status != 'completed' or not job.output_file:
+            return Response(
+                {"error": "Output file is not available yet"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        return Response({
+            "download_url": request.build_absolute_uri(job.output_file.url)
+        })
--- main/urls.py
+++ main/urls.py
@@ -1,17 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from api import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-
-router = routers.DefaultRouter()
-router.register(r'v1/test', views.ApiViewSet, basename='api')
-
-# Wire up API using automatic URL routing.
-# Additionally, include login URLs for the browsable API.
 
 urlpatterns = [
-    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+    path('api/', include('api.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
