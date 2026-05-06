92e757d6cebd9ec47bb9a0de7487cd5bfc3cf564
Django Heimgartner <django.heimgartner@gmail.com>
2026-05-06 12:01:26 +0200

    feat: add multi report support in API

--- app/api/utils/file_handling.py
+++ app/api/utils/file_handling.py
@@ -57,6 +57,9 @@ def match_output_cols(input_cols: str) -> str:
             output_cols.append('clientcode')
         elif col.startswith('report='):
             output_cols.append('processed_report')
+        elif col.split('=', 1)[0].strip().startswith('report_'):
+            suffix = col.split('=', 1)[0].strip()[len('report_') :]
+            output_cols.append(f'processed_report_{suffix}')
         elif '=' in col:
             # Keep other columns unchanged (extract the column name after '=')
             column_name = col.split('=', 1)[1].strip()
