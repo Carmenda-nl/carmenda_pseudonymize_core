3ba5ecef6c8e50bef1d2ed655d20f0fcef82d192
Django Heimgartner <django.heimgartner@gmail.com>
2026-06-12 13:54:29 +0200

    reorganise core

---
+++ app/core/deduce/__init__.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------------------------ #
+# Copyright (c) 2026 Carmenda. All rights reserved.                                                #
+# This program is distributed under the terms of the GNU General Public License: GPL-3.0-or-later  #
+# ------------------------------------------------------------------------------------------------ #
+
+"""Deduce subpackage for de-identification of medical text."""
+
+from core.deduce.deidentify import DeidentifyHandler
+
+__all__ = ['DeidentifyHandler']
